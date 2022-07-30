804661391652a1451b509ddb2b266a386dec1cc8
Salim <78372105+Feanor96@users.noreply.github.com>
2022-07-31 00:46:30 +0530

    refined my code

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,38 +1,49 @@
 const { StatusCodes } = require('http-status-codes');
 const taskModel = require('../models/Task');
+const { BadRequestError, NotFoundError } = require('../errors');
 
 const getAllTasks = async (req, res) => {
-    const task = await taskModel.find({});
+    const task = await taskModel.find({ createdBy: req.user.userId });
     res.status(StatusCodes.OK).json({ task });
 }
 
 const getTask = async (req, res) => {
-    const { id: taskId } = req.params;
-    const task = await taskModel.findOne({ _id: taskId });
+    const { user: {userId}, params: {id: taskId} } = req;
+    const task = await taskModel.findOne({ _id: taskId, createdBy: userId });
     if (!task) {
-        res.status(StatusCodes.NOT_FOUND).json({ err: `Task with id ${taskId} not found` });
+        throw new NotFoundError(`No task with id ${taskId}`);
     }
     res.status(StatusCodes.OK).json({ task });
 }
 
 const createTask = async (req, res) => {
+    req.body.createdBy = req.user.userId
     const task = await taskModel.create(req.body);
-    res.status(StatusCodes.OK).json({ task });
+    res.status(StatusCodes.CREATED).json({ task });
 }
 
 const updateTask = async (req, res) => {
-    const { id: taskId } = req.params;
-    const task = await taskModel.findByIdAndUpdate({ _id: taskId}, req.body, {
+    const { user: {userId}, params: {id: taskId} } = req;
+    const task = await taskModel.findByIdAndUpdate({ _id: taskId, createdBy: userId}, req.body, {
         new: true,
         runValidators: true,
     });
+    if (task === '') {
+        throw new BadRequestError('Task field cannot be empty');
+    }
+    if (!task) {
+        throw new NotFoundError(`No task found wit id ${taskId}`);
+    }
     res.status(StatusCodes.OK).json({ task });
 }
 
 const deleteTask = async (req, res) => {
-    const { id: taskId } = req.params;
-    const task = await taskModel.findByIdAndDelete({ _id: taskId });
-    res.status(StatusCodes.OK).json({ task });
+    const { user: { userId }, params: { id: jobId } } = req
+    const task = await taskModel.findByIdAndDelete({ _id: taskId, createdBy: userId });
+    if (!task) {
+        throw new NotFoundError(`No task with id ${taskId} found`);
+    }
+    res.status(StatusCodes.OK).json({});
 }
 
 module.exports = {
--- models/Task.js
+++ models/Task.js
@@ -2,14 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
-    task: {
-        type: String,
-        required: [true, ['Please provide a task name']],
+        task: {
+            type: String,
+            required: [true, ['Please provide a task name']],
+        },
+        completed: {
+            type: Boolean,
+            default: false,
+        },
+        createdBy: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User', 
+            required: [true, 'Please provide a user'],
+        }
     },
-    completed : {
-        type: Boolean,
-        default: false,
+    {
+        timestamps: true
     }
-})
+)
 
 module.exports = mongoose.model('TaskSchema', TaskSchema);
